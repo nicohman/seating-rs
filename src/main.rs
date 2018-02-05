cb87ecbecce7784ee448c56d937338b3348cb765
nicohman <nico.hickman@gmail.com>
2018-02-04 16:51:39 -0800

    Add manual association table editing, fix sorting bug

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use std::collections::HashMap;
 use std::cmp::Ordering;
 use std::fs::File;
 use std::io::prelude::*;
+use std::io::Read;
+use std::io;
 struct Pod {
     people: Vec<String>,
 }
@@ -42,10 +44,75 @@ fn main() {
                 }
             }
         }
+        relations = custom_rels(relations);
         let mut res = create_assign(relations, class);
         for r in res.pods {
             println!("Pod: {:?}", r.people);
         }
+    }
+}
+fn custom_rels(
+    rels: HashMap<String, HashMap<String, i32>>,
+) -> HashMap<String, HashMap<String, i32>> {
+    let mut nrels = rels.clone();
+    println!("Would you like to create a custom association?");
+    print!("(y/n)>");
+    io::stdout().flush().unwrap();
+
+    let mut buf = String::new();
+    io::stdin().read_line(&mut buf).unwrap();
+    if buf.trim() == "y" || buf.trim() == "yes" {
+        let mut con = true;
+        while con {
+            print!("First person: >");
+            io::stdout().flush().unwrap();
+
+            let mut first = String::new();
+            io::stdin().read_line(&mut first).unwrap();
+            print!("Second person: >");
+            io::stdout().flush().unwrap();
+
+            let mut sec = String::new();
+            io::stdin().read_line(&mut sec).unwrap();
+            print!("Amount to affect the relations by >");
+            io::stdout().flush().unwrap();
+
+            let mut amount = String::new();
+            io::stdin().read_line(&mut amount).unwrap();
+            {
+                let mut got = nrels.get_mut(first.trim());
+
+                if got.is_some() {
+                    *got.unwrap().entry(String::from(sec.trim())).or_insert(0) +=
+                        amount.trim().parse::<i32>().unwrap();
+                    println!("Association edited!");
+
+                } else {
+                    println!("That's not a valid person!");
+                }
+            }
+            let mut ru = nrels.get_mut(sec.trim());
+            if ru.is_some() {
+                let mut rr = ru.unwrap().entry(String::from(first.trim())).or_insert(0);
+                *rr += amount.trim().parse::<i32>().unwrap();
+            }
+            println!("Would you like edit another association?");
+            print!("(y/n");
+            io::stdout().flush().unwrap();
+            let mut chos = String::new();
+            io::stdin().read_line(&mut chos).unwrap();
+            if chos.trim() == "y" || chos.trim() == "yes" {
+                //Loop continues
+            } else if chos.trim() == "n" || chos.trim() == "no" {
+                con = false;
+            }
+
+        }
+        return nrels;
+    } else if buf.trim() == "n" || buf.trim() == "no" {
+        return nrels;
+    } else {
+        return custom_rels(nrels);
     }
 }
 fn create_assign(rels: HashMap<String, HashMap<String, i32>>, class: Vec<String>) -> Arrange {
@@ -54,17 +121,36 @@ fn create_assign(rels: HashMap<String, HashMap<String, i32>>, class: Vec<String>
     let i = 0;
     while 0 < self_c.len() {
         let p = String::from(self_c[i].as_ref());
-        let mut cur = rels.get(&p).unwrap();
-        self_c.sort_by(|a, b| {
+        let cur = rels.get(&p).unwrap();
+        self_c.sort_unstable_by(|a, b| {
             let ag = cur.get(a);
             if ag.is_some() {
                 let bg = cur.get(b);
                 if bg.is_some() {
-                    return ag.unwrap().cmp(bg.unwrap());
+                    return bg.unwrap().cmp(ag.unwrap());
+                } else {
+                    if ag.unwrap() > &0 {
+                        return Ordering::Less;
+                    } else {
+                        return Ordering::Greater;
+                    }
+                }
+            } else {
+                let bg = cur.get(b);
+                if bg.is_some() {
+                    if bg.unwrap() > &0 {
+                        return Ordering::Greater;
+                    } else {
+                        return Ordering::Less;
+                    }
                 }
             }
             return Ordering::Equal;
         });
+        //self_c.reverse();
+        for d in &self_c {
+            // println!("for {}, {}{:?}", p, d, cur.get(d));
+        }
         self_c.remove(i);
         let fi = self_c.pop();
         let mut fi_d = String::new();
@@ -80,10 +166,14 @@ fn create_assign(rels: HashMap<String, HashMap<String, i32>>, class: Vec<String>
         } else {
             sec_d = sec.unwrap();
         }
-        pods.push(Pod {
-            people: vec![p, fi_d, sec_d],
-        });
-        
+        if p == fi_d {
+            fi_d = String::from("");
+        }
+        if p == sec_d {
+            sec_d = String::from("");
+        }
+        pods.push(Pod { people: vec![p, fi_d, sec_d] });
+
     }
     Arrange {
         pods: pods,
